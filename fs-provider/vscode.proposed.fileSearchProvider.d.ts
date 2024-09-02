21332451859f1cfcf6662346f56f062ffd1d4058
Martin Aeschlimann <martinae@microsoft.com>
2024-09-02 11:38:28 +0200

    fix search provider returns invalid URIs

--- fs-provider/vscode.proposed.fileSearchProvider.d.ts
+++ fs-provider/vscode.proposed.fileSearchProvider.d.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 declare module 'vscode' {
+
 	// https://github.com/microsoft/vscode/issues/73524
 
 	/**
@@ -12,6 +13,12 @@ declare module 'vscode' {
 	export interface FileSearchQuery {
 		/**
 		 * The search pattern to match against file paths.
+		 * To be correctly interpreted by Quick Open, this is interpreted in a relaxed way. The picker will apply its own highlighting and scoring on the results.
+		 *
+		 * Tips for matching in Quick Open:
+		 * With the pattern, the picker will use the file name and file paths to score each entry. The score will determine the ordering and filtering.
+		 * The scoring prioritizes prefix and substring matching. Then, it checks and it checks whether the pattern's letters appear in the same order as in the target (file name and path).
+		 * If a file does not match at all using our criteria, it will be omitted from Quick Open.
 		 */
 		pattern: string;
 	}
@@ -48,11 +55,7 @@ declare module 'vscode' {
 		 * @param options A set of options to consider while searching files.
 		 * @param token A cancellation token.
 		 */
-		provideFileSearchResults(
-			query: FileSearchQuery,
-			options: FileSearchOptions,
-			token: CancellationToken
-		): ProviderResult<Uri[]>;
+		provideFileSearchResults(query: FileSearchQuery, options: FileSearchOptions, token: CancellationToken): ProviderResult<Uri[]>;
 	}
 
 	export namespace workspace {
