bd808cdb55696b34545a3781faa18f3855fc19eb
Martin Aeschlimann <martinae@microsoft.com>
2024-10-30 14:53:00 +0100

    apply learnings from wsl download fix

--- src/server/download.ts
+++ src/server/download.ts
@@ -26,14 +26,13 @@ async function getLatestBuild(quality: 'stable' | 'insider'): Promise<DownloadIn
 export async function getDownloadURL(quality: 'stable' | 'insider', commit: string): Promise<string | undefined> {
 	return new Promise((resolve, reject) => {
 		const url = `https://update.code.visualstudio.com/commit:${commit}/web-standalone/${quality}`;
-		const httpLibrary = url.startsWith('https') ? https : http;
-		httpLibrary.get(url, { method: 'HEAD', ...getAgent(url) }, res => {
-			console.log(res.statusCode, res.headers.location);
+		https.get(url, { method: 'HEAD', ...getAgent(url) }, res => {
 			if ((res.statusCode === 301 || res.statusCode === 302 || res.statusCode === 307) && res.headers.location) {
 				resolve(res.headers.location);
 			} else {
 				resolve(undefined);
 			}
+			res.resume(); // Discard response body
 		});
 	});
 }
@@ -68,6 +67,7 @@ async function downloadAndUntar(downloadUrl: string, destination: string, messag
 
 				received += chunk.length;
 			});
+			res.on('error', reject);
 			res.on('end', () => {
 				if (timeout) {
 					clearTimeout(timeout);
